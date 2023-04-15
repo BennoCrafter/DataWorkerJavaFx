787fd3e812c3f4ed50649260c29edbad4feec889
Joachim Baumeister <joachim.baumeister@denkbares.com>
2023-04-15 10:03:05 +0200

    Initial commit of the FX branch

--- src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerController.java
+++ src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerController.java
@@ -1,14 +1,46 @@
 package de.bennocrafter.dataworker.dataworkerfx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.ContextMenuEvent;
+
+import de.bennocrafter.dataworker.core.Entry;
 
 public class DataWorkerController {
+
+	@FXML
+	private TableView<Entry> entryBaseTable;
+
 	@FXML
 	private Label welcomeText;
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("Welcome to JavaFX Application!");
+	void onContextEntryBaseTable(ContextMenuEvent event) {
 	}
+
+	@FXML
+	void onHelloButtonClick(ActionEvent event) {
+		TableColumn<Entry, String> nameColumn = new TableColumn<>("Name");
+		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+		TableColumn<Entry, Integer> ageColumn = new TableColumn<>("Age");
+		ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+		entryBaseTable.getColumns().add(nameColumn);
+		entryBaseTable.getColumns().add(ageColumn);
+		Entry textEntry = new Entry();
+		textEntry.addValueFor("name", "joba").addValueFor("age", "50");
+		entryBaseTable.getItems().add(textEntry);
+
+	}
+
+	@FXML
+	void onSortTable(ActionEvent event) {
+
+	}
+
 }
--- src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerFX.java
+++ src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerFX.java
@@ -10,12 +10,13 @@ import javafx.stage.Stage;
 public class DataWorkerFX extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(DataWorkerFX.class.getResource("hello-view.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(DataWorkerFX.class.getResource("dataworker-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 		stage.setTitle("Hello!");
 		stage.setScene(scene);
 		stage.show();
 	}
+
 
 	public static void main(String[] args) {
 		launch();
