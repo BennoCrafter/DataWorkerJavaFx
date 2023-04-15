f0ecc631a360d7eafb8ffddad4eb32d19624c0a2
Joachim Baumeister <joachim.baumeister@denkbares.com>
2023-04-15 15:24:22 +0200

    TableView with view-only feature.

--- src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerController.java
+++ src/main/java/de/bennocrafter/dataworker/dataworkerfx/DataWorkerController.java
@@ -1,30 +1,80 @@
 package de.bennocrafter.dataworker.dataworkerfx;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.VBox;
 
 import de.bennocrafter.dataworker.core.Entry;
 import de.bennocrafter.dataworker.core.EntryBase;
 import de.bennocrafter.dataworker.io.CSVDataWorkerReader;
 
-public class DataWorkerController {
+public class DataWorkerController implements Initializable {
+	private List<String> recentFiles = new ArrayList<>();
 
+	private static final String DATAWORKER_PROPERTIES = "dataworker.properties";
 	@FXML
 	private TableView<Entry> entryBaseTable;
 
 	@FXML
-	private Button loadButton;
+	private VBox recentBasesPane;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		updateRecentBasesPane();
+	}
+	private void updateRecentBasesPane() {
+		Properties properties = new Properties();
+		try (InputStream input = new FileInputStream(DATAWORKER_PROPERTIES)) {
+			properties.load(input);
+			String recentfiles = properties.getProperty("recentfiles");
+			if (recentfiles != null) {
+				this.recentFiles = Arrays.asList(recentfiles.split(","));
+				for (String file: this.recentFiles) {
+					Button button = new Button(file);
+					button.setOnAction(event -> {
+						try {
+							loadAndRefresh(file);
+						}
+						catch (Exception e) {
+							throw new RuntimeException(e);
+						}
+					});
+
+					recentBasesPane.getChildren().add(button);
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
 
 	@FXML
-	void onLoadButtonClick(ActionEvent event) throws Exception {
-		String fileName = "Weinbuecher.csv";
+	void onQuitAction(ActionEvent event) {
+		Platform.exit();
+	}
+
+	private void loadAndRefresh(String fileName) throws Exception {
+		entryBaseTable.getColumns().clear();
+		entryBaseTable.getItems().clear();
 		CSVDataWorkerReader r = new CSVDataWorkerReader();
-		EntryBase base = r.read(fileName, "Weinbuecher");
+		EntryBase base = r.read(fileName, fileName);
 
 		// define header of the table
 		for (String attribute: base.getAttributes()) {
@@ -40,6 +90,6 @@ public class DataWorkerController {
 			entryBaseTable.getItems().add(e);
 		}
 		entryBaseTable.setEditable(true);
-	}
 
+	}
 }
